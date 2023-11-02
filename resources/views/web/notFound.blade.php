15c9d16f75c747b3c9885c680fc84778337c932d
fandi-ahmad <fandi4160@gmail.com>
2023-11-02 23:00:50 +0800

    change code for data not found, from 400 to 404

--- resources/views/web/notFound.blade.php
+++ resources/views/web/notFound.blade.php
@@ -2,7 +2,7 @@
   <x-navbar />
 
   <x-container class="text-center text-gray-700 pt-10">
-    <div class="text-6xl font-bold">400</div>
+    <div class="text-6xl font-bold">404</div>
     <div class="text-3xl font-semibold">Data Not Found</div>
   </x-container>
 </x-template>
